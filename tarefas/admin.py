eac7ccd79b545548831df26d9c8adadbef30d829
Graziele <c.graziele.rodrigues@gmail.com>
2021-07-14 16:31:47 -0300

    criação bancos de dados

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,3 +1,8 @@
+from tarefas.models import Tarefa
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Tarefa)
+
+class TarefaAdmin(admin.ModelAdmin):
+    list_display=['id', 'conteudo', 'data',]
--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tarefa(models.Model):
+    conteudo = models.CharField(max_length=100)
+    data=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.conteudo)
+        
